54697d113ac984e48e5b8540bb33e12d1391b0d7
Allison Aguirre <alli.aguirre95@gmail.com>
2026-09-04 21:06:16 -0600

    hello world

---
+++ README.md
@@ -0,0 +1,17 @@
+# Overview
+
+This program displays "Hello World!" in the console.
+
+The purpose of this software is to practice creating, compiling, and running a simple C++ program that prints a message to the console.
+
+[Software Demo Video](https://youtu.be/GFbnVBoap6w)
+
+# Development Environment
+
+The tools used to develop this software were VS Code, MSYS2, the g++ compiler, and Git.
+The programming language used was C++.
+
+# Useful Websites
+
+* [GeeksforGeeks](https://www.geeksforgeeks.org/cpp/writing-first-c-program-hello-world-example/)
+* [Microsoft](https://devblogs.microsoft.com/cppblog/cpp-tutorial-hello-world/)
---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+/*
+int main() {
+    const short minValue = 1;
+    const short maxValue = 6;
+
+    srand(time(0));
+    short first = rand() % (maxValue - minValue + 1) + minValue;
+    short second = rand() % (maxValue - minValue + 1) + minValue;
+    cout << first << ", " << second;
+    return 0;
+}
+*/
+
+/*
+int main() {
+    cout << "Result: Enter radius: ";
+    double radius;
+    cin >> radius;
+    const double pi = 3.14;
+    double area = pi * pow(radius, 2);
+    cout << "Area: " << area;
+    return 0;
+}
+*/
+
+/*
+ int main() {
+    cout << "Fahrenheit: ";
+    int fahrenheit;
+    cin >> fahrenheit;
+    double celsius = (fahrenheit - 32) / 1.8;
+    cout << "Celsius: " << celsius;
+    return 0;
+}
+ */
+
+
+/*
+int main() {
+    // This is a simple program that swaps the values of two integers and prints the result.
+    int a = 1;
+    int b = 2;
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << a; 
+
+    return 0;
+}
+*/
+
+/*
+int main() {
+    double sales = 95000;
+    cout << "Sales: $" << sales << endl;
+
+    const double stateTaxRate = 0.04;
+    double stateTax = sales * stateTaxRate;
+    cout << "State Tax: $" << stateTax << endl;
+
+    const double countyTaxRate = 0.02;
+    double countyTax = sales * countyTaxRate;
+    cout << "County Tax: $" << countyTax << endl;
+
+    double totalTax = stateTax + countyTax;
+    cout << "Total Tax: $" << totalTax << endl;
+
+    return 0;
+}
+
+/*
+int mainS() {
+    const double pi = 3.14;
+    return 0;
+}*/
